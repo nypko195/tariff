3e9d918ad0eed3a6cbfbb5a680ae527076df0bb9
Aleksey <nypko195@bk.ru>
2021-03-15 16:56:32 +0400

    completed media

--- js/script.js
+++ js/script.js
@@ -1,2 +1,71 @@
 "use strict";
+window.addEventListener('DOMContentLoaded', () => { 
+
+   //burger
+   const burger = document.querySelector('.header__burger'),
+         burgerMenu = document.querySelector('.header__menu'),
+         content = document.querySelector('body');
+
+   burger.addEventListener('click', (e) => {
+      if (e.target === burger ) {
+            burger.classList.toggle('active');
+            burgerMenu.classList.toggle('active');   
+            content.classList.toggle('lock');
+         }
+      });
+
+   const home = document.getElementById('drop'),
+         subBurgerBtn = document.querySelector('.header__menu-sublist'),
+         burgerBtn = document.getElementById('two'); 
+
+      home.addEventListener('click', (e) => {
+      if (e.target === home) {
+         subBurgerBtn.classList.toggle('active');
+         home.classList.toggle('active');
+         if (home.classList.contains('active')) {
+            home.style.paddingBottom = '190px';
+         } else {
+            home.style.paddingBottom = '20px';
+         }
+      }
+      e.preventDefault();
+   });
+   //бургер
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+
+   
+
+
+
+
 
