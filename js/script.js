7d7a23fad2ec070ce947befe85b426823112f870
Aleksey <nypko195@bk.ru>
2021-03-22 19:23:38 +0400

    btn

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 window.addEventListener('DOMContentLoaded', () => { 
 
    //burger
+   //открытие бургера
    const burger = document.querySelector('.header__burger'),
          burgerClick = document.querySelector('span'),
          burgerMenu = document.querySelector('.header__menu'),
@@ -15,15 +16,17 @@ window.addEventListener('DOMContentLoaded', () => {
          }
       });
 
+   
+   //кнопка домашний интернет
    const home = document.getElementById('btn-1'),
          subBurgerBtn = document.querySelector('.header__menu-sublist'),         
-         burgerActive = document.querySelector('.header__menu');         
+         headerBurger = document.querySelector('.header__burger');          
 
       home.addEventListener('click', (e) => {
       if (e.target === home) {
          subBurgerBtn.classList.toggle('active');
-         home.classList.toggle('active');
-         if (home.classList.contains('active')) {
+         home.classList.toggle('active');         
+         if (home.classList.contains('active') && headerBurger.classList.contains('active')) {
             home.style.paddingBottom = '190px';
          }         
          else {
@@ -33,66 +36,46 @@ window.addEventListener('DOMContentLoaded', () => {
       e.preventDefault();
    });
 
-   // home.addEventListener('mouseover', (e) => {
-   //    if (e.target === home) {
-   //       subBurgerBtn.classList.toggle('active'); 
-   //       home.classList.toggle('active');
-   //       if (home.classList.contains('active')) {
-   //          home.style.paddingBottom = '190px';
-   //       } 
-   //    }
-   // });
-
-   // home.addEventListener('mouseout', (e) => {            
-   //       if (e.target === home && e.relatedTarget === burgerActive) {
-   //          subBurgerBtn.classList.toggle('active'); 
-   //          home.classList.toggle('active');
-   //          if (!home.classList.contains('active')) {
-   //             home.style.paddingBottom = '20px';
-   //          } 
-         
-   //    }    
-   // });
-   //бургер
-
    //валидация
    const form = document.querySelector('.header__bid-form');
    const userTel = document.getElementById('input1'),
          userName = document.getElementById('input2'),
          userAdress = document.getElementById('input3');       
          
-
+   //обработчик события на отправку формы
    form.addEventListener('submit', (e) => {
-      function validationTel(user) {
-         /*const regTel = /\d/g;*/
-         const regTel = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
-         if (!regTel.test(user.value)) {
-            user.classList.add('novalid');
-            console.log('-');            
-         } else {
-            console.log('+');
-            user.classList.remove('novalid');
-            user.classList.add('valid');
-         }
-      }
-      
-
-      function validationName(name) {
-         const regName = /\w\D/g;
-         if(!regName.test(name.value)) {
-            console.log('-');
-            name.classList.add('novalid');
-         } else {
-            console.log('+');
-            name.classList.remove('novalid');
-            name.classList.add('valid');
-         }
-      }  
-      
       validationTel(userTel);
-      validationName(userName);      
+      validationName(userName); 
 
       e.preventDefault();
-   });   
+   });
+   //проверка телефона
+   function validationTel(user) {
+      /*const regTel = /\d/g;*/
+      const regTel = /^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/;
+      if (!regTel.test(user.value)) {
+         user.classList.add('novalid');
+         console.log('-');            
+      } else {
+         console.log('+');
+         user.classList.remove('novalid');
+         user.classList.add('valid');
+      }
+   }
+   
+   //проверка имени
+   function validationName(name) {
+      const regName = /[а-яА-Яa-zA-Z]/;
+      if(!regName.test(name.value)) {
+         console.log('-');
+         name.classList.add('novalid');
+      } else {
+         console.log('+');
+         name.classList.remove('novalid');
+         name.classList.add('valid');
+      }
+   }  
+   
+   
 });
 
